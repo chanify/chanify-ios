be04d3b23bd422707d3e6bcb92ab162d46dee11b
wizjin <wizjin@icloud.com>
2021-06-02 08:41:31 +0800

    refactor: use datasource in channel list.

--- OSX/Views/Channels/CHChannelsView.m
+++ OSX/Views/Channels/CHChannelsView.m
@@ -9,16 +9,20 @@
 #import <Masonry/Masonry.h>
 #import "CHChannelCellView.h"
 #import "CHUserDataSource.h"
+#import "CHMessageModel.h"
 #import "CHLogic+OSX.h"
 #import "CHRouter+OSX.h"
 #import "CHTheme.h"
 
 static NSString *const cellIdentifier = @"CHChannelCellView";
 
-@interface CHChannelsView () <NSCollectionViewDelegate, NSCollectionViewDataSource, NSCollectionViewDelegateFlowLayout, CHLogicDelegate>
+typedef NSCollectionViewDiffableDataSource<NSString *, CHChannelModel *> CHChannelDataSource;
+typedef NSDiffableDataSourceSnapshot<NSString *, CHChannelModel *> CHChannelDiffableSnapshot;
+
+@interface CHChannelsView () <NSCollectionViewDelegate, NSCollectionViewDelegateFlowLayout, CHLogicDelegate>
 
 @property (nonatomic, readonly, strong) NSCollectionView *listView;
-@property (nonatomic, readonly, strong) NSArray<CHChannelModel *> *items;
+@property (nonatomic, readonly, strong) CHChannelDataSource *dataSource;
 
 @end
 
@@ -27,23 +31,29 @@ static NSString *const cellIdentifier = @"CHChannelCellView";
 - (instancetype)initWithFrame:(NSRect)frameRect {
     if (self = [super initWithFrame:frameRect]) {
         self.backgroundColor = CHTheme.shared.groupedBackgroundColor;
+        self.hasVerticalScroller = YES;
 
         NSCollectionViewFlowLayout *layout = [NSCollectionViewFlowLayout new];
         layout.scrollDirection = NSCollectionViewScrollDirectionVertical;
         layout.minimumLineSpacing = 1;
         NSCollectionView *listView = [NSCollectionView new];
-        [self addSubview:(_listView = listView)];
+        _listView = listView;
         [listView registerClass:CHChannelCellView.class forItemWithIdentifier:cellIdentifier];
         listView.backgroundColors = @[self.backgroundColor];
         listView.collectionViewLayout = layout;
         listView.allowsMultipleSelection = NO;
         listView.allowsEmptySelection = NO;
         listView.selectable = YES;
-        listView.dataSource = self;
         listView.delegate = self;
         self.documentView = listView;
-        self.hasVerticalScroller = YES;
-        
+
+        _dataSource = [[CHChannelDataSource alloc] initWithCollectionView:listView itemProvider:^NSCollectionViewItem * _Nullable(NSCollectionView * collectionView, NSIndexPath * indexPath, CHChannelModel * model) {
+            CHChannelCellView *item = [collectionView makeItemWithIdentifier:cellIdentifier forIndexPath:indexPath];
+            if (item != nil) {
+                item.model = model;
+            }
+            return item;
+        }];
         [CHLogic.shared addDelegate:self];
     }
     return self;
@@ -59,9 +69,12 @@ static NSString *const cellIdentifier = @"CHChannelCellView";
 }
 
 - (void)reloadData {
-    _items = [CHLogic.shared.userDataSource loadChannels];
-    [self.listView reloadData];
-    if (self.listView.selectionIndexes.count <= 0 && self.items.count > 0) {
+    NSArray<CHChannelModel *> *items = [CHLogic.shared.userDataSource loadChannels];
+    CHChannelDiffableSnapshot *snapshot = [CHChannelDiffableSnapshot new];
+    [snapshot appendSectionsWithIdentifiers:@[@"main"]];
+    [snapshot appendItemsWithIdentifiers:[items sortedArrayUsingSelector:@selector(messageCompare:)]];
+    [self.dataSource applySnapshot:snapshot animatingDifferences:NO];
+    if (self.listView.selectionIndexes.count <= 0 && items.count > 0) {
         NSSet<NSIndexPath *> *indexPaths = [NSSet setWithObject:[NSIndexPath indexPathForItem:0 inSection:0]];
         [self.listView selectItemsAtIndexPaths:indexPaths scrollPosition:NSCollectionViewScrollPositionNone];
         [self collectionView:self.listView didSelectItemsAtIndexPaths:indexPaths];
@@ -70,23 +83,10 @@ static NSString *const cellIdentifier = @"CHChannelCellView";
 
 #pragma mark - NSCollectionViewDelegate
 - (void)collectionView:(NSCollectionView *)collectionView didSelectItemsAtIndexPaths:(NSSet<NSIndexPath *> *)indexPaths {
-    CHChannelModel *item = [self.items objectAtIndex:indexPaths.anyObject.item];
+    CHChannelModel *item = [self.dataSource itemIdentifierForIndexPath:indexPaths.anyObject];
     if (item != nil) {
         [CHRouter.shared routeTo:@"/page/channel" withParams:@{ @"cid": item.cid, @"show": @"detail" }];
     }
-}
-
-#pragma mark - NSCollectionViewDataSource
-- (NSInteger)collectionView:(NSCollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return self.items.count;
-}
-
-- (NSCollectionViewItem *)collectionView:(NSCollectionView *)collectionView itemForRepresentedObjectAtIndexPath:(NSIndexPath *)indexPath {
-    CHChannelCellView *item = [collectionView makeItemWithIdentifier:cellIdentifier forIndexPath:indexPath];
-    if (item != nil) {
-        item.model = [self.items objectAtIndex:indexPath.item];
-    }
-    return item;
 }
 
 #pragma mark - NSCollectionViewDelegateFlowLayout
@@ -104,7 +104,29 @@ static NSString *const cellIdentifier = @"CHChannelCellView";
 }
 
 - (void)logicMessagesUpdated:(NSArray<NSString *> *)mids {
-    [self reloadData];
+    CHUserDataSource *usrDS = CHLogic.shared.userDataSource;
+    CHChannelDiffableSnapshot *snapshot = self.dataSource.snapshot;
+    NSArray<CHChannelModel *> *items = [snapshot itemIdentifiersInSectionWithIdentifier:@"main"];
+    NSHashTable *reloadItems = [NSHashTable weakObjectsHashTable];
+    for (NSString *mid in mids) {
+        CHMessageModel *model = [usrDS messageWithMID:mid];
+        NSString *cid = model.channel.base64;
+        for (CHChannelModel *c in items) {
+            if ([c.cid isEqualToString:cid]) {
+                c.mid = mid;
+                [reloadItems addObject:c];
+                break;
+            }
+        }
+    }
+    if (reloadItems.count != 1 || reloadItems.anyObject != items.firstObject) {
+        // TODO: sort items
+        [snapshot deleteSectionsWithIdentifiers:@[@"main"]];
+        [snapshot appendSectionsWithIdentifiers:@[@"main"]];
+        [snapshot appendItemsWithIdentifiers:[items sortedArrayUsingSelector:@selector(messageCompare:)]];
+    }
+    [snapshot reloadItemsWithIdentifiers:reloadItems.allObjects];
+    [self.dataSource applySnapshot:snapshot animatingDifferences:YES];
 }
 
 - (void)logicMessagesUnreadChanged:(NSNumber *)unread {
