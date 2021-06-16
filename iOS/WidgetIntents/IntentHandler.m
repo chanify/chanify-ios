3b391d892acd28fac6763010fc6ae32bb1285fb2
wizjin <wizjin@icloud.com>
2021-06-16 23:49:36 +0800

    refactor: layout shortcuts widget.

--- iOS/WidgetIntents/IntentHandler.m
+++ iOS/WidgetIntents/IntentHandler.m
@@ -7,12 +7,22 @@
 
 #import "IntentHandler.h"
 #import "ShortcutsConfigurationIntent.h"
+#import "NSString+CHLocalized.h"
 
 @interface IntentHandler () <ShortcutsConfigurationIntentHandling>
+
+@property (nonatomic, readonly, strong) NSArray<NSString *> *types;
 
 @end
 
 @implementation IntentHandler
+
+- (instancetype)init {
+    if (self = [super init]) {
+        _types = @[@"action", @"channel", @"node"];
+    }
+    return self;
+}
 
 - (id)handlerForIntent:(INIntent *)intent {
     // This is the default implementation.  If you want different objects to handle different intents,
@@ -25,20 +35,31 @@
 - (void)provideEntriesOptionsCollectionForShortcutsConfiguration:(ShortcutsConfigurationIntent *)intent withCompletion:(void (^)(INObjectCollection<EntryType *> * _Nullable entriesOptionsCollection, NSError * _Nullable error))completion {
     if (completion) {
         NSMutableArray<EntryType *> *items = [NSMutableArray new];
-        [items addObject:[self entryWithIdentifier:@"action.scan" name:@"Scan"]];
-        [items addObject:[self entryWithIdentifier:@"channel.sys.none" name:@"Uncategorized"]];
-        [items addObject:[self entryWithIdentifier:@"channel.sys.device" name:@"Devices"]];
+        [items addObject:[self entryWithIdentifier:@"action.scan"]];
+        [items addObject:[self entryWithIdentifier:@"channel.sys.none"]];
+        [items addObject:[self entryWithIdentifier:@"channel.sys.device"]];
         completion([[INObjectCollection alloc] initWithItems:items], nil);
     }
 }
 
 - (nullable NSArray<EntryType *> *)defaultEntriesForShortcutsConfiguration:(ShortcutsConfigurationIntent *)intent {
-    return  @[[self entryWithIdentifier:@"action.scan" name:@"Scan"]];
+    return  @[[self entryWithIdentifier:@"action.scan"]];
 }
 
 #pragma mark - Private Methods
-- (EntryType *)entryWithIdentifier:(NSString *)identifier name:(NSString *)name {
-    return [[EntryType alloc] initWithIdentifier:identifier displayString:name];
+- (EntryType *)entryWithIdentifier:(NSString *)identifier {
+    NSString *title = @"";
+    for (NSString *name in self.types) {
+        NSInteger index = name.length;
+        if ([identifier hasPrefix:name] && [identifier characterAtIndex:index] == '.') {
+            title = [NSString stringWithFormat:@"%@: %@", name.localized, [identifier substringFromIndex:index + 1].localized];
+            break;
+        }
+    }
+    EntryType *entry = [[EntryType alloc] initWithIdentifier:identifier displayString:title];
+    entry.icon = @"";
+    entry.link = @"chanify:///action/scan";
+    return entry;
 }
 
 
--- iOS/Widgets/Widgets/ShortcutsWidget.swift
+++ iOS/Widgets/Widgets/ShortcutsWidget.swift
@@ -30,31 +30,37 @@ struct ShortcutsProvider: IntentTimelineProvider {
     }
 }
 
+struct EntryItemView : View {
+    var entry: EntryType
+    
+    var body: some View {
+        Link(destination: URL(string: (entry.link ?? "")) ?? URL(string: "chanify:///home")!) {
+            IconView(icon: entry.icon)
+        }
+    }
+}
+
 struct ShortcutsEntryView : View {
     @Environment(\.colorScheme) var colorScheme
 
     var entry: ShortcutsProvider.Entry
 
     var body: some View {
-        ZStack {
-            Color(.systemBackground)
-            HStack {
-                Link(destination:URL(string: "chanify:///home")!) {
-                    Image(systemName: "qrcode")
-                        .resizable().scaledToFit()
-                        .frame(width: 32, height: 32, alignment: .center)
+        GeometryReader { geometry in
+            ZStack {
+                Color(.systemBackground)
+                if let entries = entry.configuration.entries {
+                    let size = geometry.size.height * 0.35
+                    let margin = CGPoint(x: (geometry.size.width - size * 4.0)/5.0, y: geometry.size.height * 0.1)
+                    let offset = CGPoint(x: size*0.5 + margin.x, y: size*0.5 + margin.y)
+                    ForEach(0..<entries.count) { i in
+                        EntryItemView(entry: entries[i])
+                            .frame(width: size, height: size, alignment: .center)
+                            .position(x: offset.x + (size + margin.x) * CGFloat(i%4), y: offset.y + (size + margin.y) * CGFloat(i/4))
+                    }
                 }
-                Link(destination:URL(string: "chanify:///action/scan")!) {
-                    Image(systemName: "qrcode.viewfinder")
-                        .resizable().scaledToFit()
-                        .frame(width: 32, height: 32, alignment: .center)
-                }
-                Link(destination:URL(string: "chanify:///channel")!) {
-                    IconView(icon: "")
-                        .frame(width: 32, height: 32, alignment: .center)
-                }
-            }
-        }.colorScheme(colorScheme.withAppearance(entry.configuration.appearance))
+            }.colorScheme(colorScheme.withAppearance(entry.configuration.appearance))
+        }
     }
 }
 
@@ -72,8 +78,28 @@ struct ShortcutsWidget: Widget {
 }
 
 //struct ShortcutsWidget_Previews: PreviewProvider {
+//    static func entry(identifier: String, display: String, icon: String) -> EntryType {
+//        let entry = EntryType(identifier: identifier, display: display)
+//        entry.icon = icon
+//        entry.link = "chanify:///action/scan"
+//        return entry
+//    }
+//
+//    static func configuration() -> ShortcutsConfigurationIntent {
+//        let c = ShortcutsConfigurationIntent();
+//        c.appearance = .dark
+//        c.entries = [
+//            entry(identifier: "action.scan", display: "Scan", icon: "sys://qrcode.viewfinder"),
+//            entry(identifier: "action.channel.001", display: "Channel", icon: ""),
+//            entry(identifier: "action.channel.002", display: "Channel", icon: ""),
+//            entry(identifier: "action.channel.003", display: "Channel", icon: ""),
+//            entry(identifier: "action.channel.004", display: "Channel", icon: "")
+//        ]
+//        return c
+//    }
+//
 //    static var previews: some View {
-//        ShortcutsEntryView(entry: ShortcutsEntry(date: Date(), configuration: ShortcutsConfigurationIntent()))
+//        ShortcutsEntryView(entry: ShortcutsEntry(date: Date(), configuration: configuration()))
 //            .previewContext(WidgetPreviewContext(family: .systemMedium))
 //    }
 //}
