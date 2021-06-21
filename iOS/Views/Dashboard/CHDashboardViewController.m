ebd5fa365d6d74dcedc13cbc2930de3224b8254e
wizjin <wizjin@icloud.com>
2021-06-22 07:27:07 +0800

    refactor: reorder panel item in dashboard.

--- iOS/Views/Dashboard/CHDashboardViewController.m
+++ iOS/Views/Dashboard/CHDashboardViewController.m
@@ -13,12 +13,21 @@
 
 typedef UICollectionViewDiffableDataSource<NSString *, CHPanelCellConfiguration *> CHDashboardDataSource;
 typedef NSDiffableDataSourceSnapshot<NSString *, CHPanelCellConfiguration *> CHDashboardDiffableSnapshot;
-typedef NSDiffableDataSourceTransaction<NSString *, CHPanelCellConfiguration *> CHDashboardDiffableTransaction;
 
 @interface CHCollectionViewDashboardLayout : UICollectionViewFlowLayout
 @end
 
 @implementation CHCollectionViewDashboardLayout
+
+- (instancetype)init {
+    if (self = [super init]) {
+        CGFloat margin = 8;
+        self.sectionInset = UIEdgeInsetsMake(margin, margin, margin, margin);
+        self.minimumInteritemSpacing = margin;
+        self.minimumLineSpacing = margin;
+    }
+    return self;
+}
 
 - (nullable UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath {
     UICollectionViewLayoutAttributes *attributes = [super layoutAttributesForItemAtIndexPath:indexPath];
@@ -34,12 +43,6 @@ typedef NSDiffableDataSourceTransaction<NSString *, CHPanelCellConfiguration *> 
     return attributeList;
 }
 
-- (UICollectionViewLayoutAttributes *)layoutAttributesForInteractivelyMovingItemAtIndexPath:(NSIndexPath *)indexPath withTargetPosition:(CGPoint)position {
-    UICollectionViewLayoutAttributes *attributes = [super layoutAttributesForInteractivelyMovingItemAtIndexPath:indexPath withTargetPosition:position];
-    attributes.alpha = 0.9;
-    return attributes;
-}
-
 #pragma mark - Private Methods
 - (void)fixLayoutAttributes:(UICollectionViewLayoutAttributes *)attributes {
     if (attributes.frame.origin.x >= self.sectionInset.left && attributes.frame.origin.x*2 < self.collectionView.frame.size.width) {
@@ -51,40 +54,28 @@ typedef NSDiffableDataSourceTransaction<NSString *, CHPanelCellConfiguration *> 
 
 @end
 
-@interface CHDashboardViewController () <UICollectionViewDelegate, UICollectionViewDelegateFlowLayout>
+@interface CHDashboardViewController () <UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDragDelegate, UICollectionViewDropDelegate>
 
 @property (nonatomic, readonly, strong) UICollectionView *collectionView;
 @property (nonatomic, readonly, strong) CHDashboardDataSource *dataSource;
-@property (nonatomic, readonly, strong) UILongPressGestureRecognizer *reorderGestureRecognizer;
-@property (nonatomic, readonly, strong) NSMutableDictionary<NSIndexPath *, NSIndexPath *> *movedIndexPaths;
 
 @end
 
 @implementation CHDashboardViewController
 
-- (instancetype)init {
-    if (self = [super init]) {
-        _movedIndexPaths = [NSMutableDictionary new];
-    }
-    return self;
-}
-
-- (void)dealloc {
-    [self.collectionView removeGestureRecognizer:self.reorderGestureRecognizer];
-}
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage systemImageNamed:@"plus.circle"] style:UIBarButtonItemStylePlain target:self action:@selector(actionCreate:)];
-    
-    @weakify(self);
-    CHCollectionViewDashboardLayout *layout = [CHCollectionViewDashboardLayout new];
-    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:layout];
+
+    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:[CHCollectionViewDashboardLayout new]];
     [self.view addSubview:(_collectionView = collectionView)];
     collectionView.alwaysBounceVertical = YES;
     collectionView.allowsSelection = NO;
     collectionView.backgroundColor = CHTheme.shared.groupedBackgroundColor;
+    collectionView.dragInteractionEnabled = YES;
+    collectionView.dragDelegate = self;
+    collectionView.dropDelegate = self;
     collectionView.delegate = self;
     [collectionView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self.view.mas_safeAreaLayoutGuideTop);
@@ -96,16 +87,6 @@ typedef NSDiffableDataSourceTransaction<NSString *, CHPanelCellConfiguration *> 
     _dataSource = [[CHDashboardDataSource alloc] initWithCollectionView:collectionView cellProvider:^CHPanelCollectionViewCell *(UICollectionView *collectionView, NSIndexPath *indexPath, CHPanelCellConfiguration *item) {
         return [collectionView dequeueConfiguredReusableCellWithRegistration:registration forIndexPath:indexPath item:item];
     }];
-    self.dataSource.reorderingHandlers.canReorderItemHandler = ^BOOL(CHPanelCellConfiguration *configuration) {
-        return YES;
-    };
-    self.dataSource.reorderingHandlers.willReorderHandler = ^(CHDashboardDiffableTransaction *transaction) {
-        @strongify(self);
-        [self.movedIndexPaths removeAllObjects];
-    };
-
-    UILongPressGestureRecognizer *reorderGestureRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(actionReorder:)];
-    [self.collectionView addGestureRecognizer:(_reorderGestureRecognizer = reorderGestureRecognizer)];
 
     NSMutableArray<CHPanelCellConfiguration *> *items = [NSMutableArray new];
     [items addObject:[CHPanelCellConfiguration cellConfiguration:@"1"]];
@@ -126,34 +107,57 @@ typedef NSDiffableDataSourceTransaction<NSString *, CHPanelCellConfiguration *> 
     [collectionView deselectItemAtIndexPath:indexPath animated:YES];
 }
 
-- (NSIndexPath *)collectionView:(UICollectionView *)collectionView targetIndexPathForMoveFromItemAtIndexPath:(NSIndexPath *)originalIndexPath toProposedIndexPath:(NSIndexPath *)proposedIndexPath {
-    if (originalIndexPath.section != proposedIndexPath.section) {
-        return originalIndexPath;
-    }
-    if (originalIndexPath.item == proposedIndexPath.item) {
-        return proposedIndexPath;
-    }
-    NSIndexPath *originalCurrent = [self.movedIndexPaths objectForKey:originalIndexPath];
-    NSIndexPath *proposedCurrent = [self.movedIndexPaths objectForKey:proposedIndexPath];
-    [self.movedIndexPaths setObject:(proposedCurrent ?: proposedIndexPath) forKey:originalIndexPath];
-    [self.movedIndexPaths setObject:(originalCurrent ?: originalIndexPath) forKey:proposedIndexPath];
-    return proposedIndexPath;
-}
-
 #pragma mark - UICollectionViewDelegateFlowLayout
-- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
-    indexPath = [self.movedIndexPaths objectForKey:indexPath] ?: indexPath;
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(CHCollectionViewDashboardLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
     CHPanelCellConfiguration *cell = [self.dataSource itemIdentifierForIndexPath:indexPath];
-    CGFloat width = self.collectionView.frame.size.width;
-    if ([cell.code integerValue]%2 == 0) {
-        return CGSizeMake(width * 0.48, 120);
+    CGFloat width = self.collectionView.frame.size.width - collectionViewLayout.sectionInset.left - collectionViewLayout.sectionInset.right;
+    if (cell.code.integerValue%2 == 0) {
+        return CGSizeMake((width - collectionViewLayout.minimumInteritemSpacing)/2, 120);
     }
     return CGSizeMake(width, 120);
 }
 
+#pragma mark - UICollectionViewDragDelegate
+- (NSArray<UIDragItem *> *)collectionView:(UICollectionView *)collectionView itemsForBeginningDragSession:(id<UIDragSession>)session atIndexPath:(NSIndexPath *)indexPath {
+    CHPanelCellConfiguration *item = [self.dataSource itemIdentifierForIndexPath:indexPath];
+    NSItemProvider *itemProvider = [[NSItemProvider alloc] initWithObject:item.code];
+    UIDragItem *dragItem = [[UIDragItem alloc] initWithItemProvider:itemProvider];
+    dragItem.localObject = item;
+    return @[dragItem];
+}
+
+#pragma mark - UICollectionViewDropDelegate
+- (void)collectionView:(UICollectionView *)collectionView performDropWithCoordinator:(id<UICollectionViewDropCoordinator>)coordinator {
+    NSIndexPath *sourceIndexPath = coordinator.items.firstObject.sourceIndexPath;
+    NSIndexPath *destinationIndexPath = coordinator.destinationIndexPath;
+    if (coordinator.items.count == 1 && sourceIndexPath != nil && destinationIndexPath != nil) {
+        CHPanelCellConfiguration *sourceItem = coordinator.items.firstObject.dragItem.localObject;
+        CHDashboardDiffableSnapshot *snapshot = self.dataSource.snapshot;
+        [snapshot deleteItemsWithIdentifiers:@[sourceItem]];
+        NSArray *items = snapshot.itemIdentifiers;
+        if (destinationIndexPath.item == 0) {
+            [snapshot insertItemsWithIdentifiers:@[sourceItem] beforeItemWithIdentifier:items.firstObject];
+        } else if (destinationIndexPath.item > 0 && destinationIndexPath.item < items.count) {
+            [snapshot insertItemsWithIdentifiers:@[sourceItem] beforeItemWithIdentifier:[items objectAtIndex:destinationIndexPath.item]];
+        } else {
+            [snapshot appendItemsWithIdentifiers:@[sourceItem]];
+        }
+        [self.dataSource applySnapshot:snapshot animatingDifferences:NO];
+        [coordinator dropItem:coordinator.items.firstObject.dragItem toItemAtIndexPath:destinationIndexPath];
+    }
+}
+
+- (BOOL)collectionView:(UICollectionView *)collectionView canHandleDropSession:(id<UIDropSession>)session {
+    return session.localDragSession != nil;
+}
+
+- (UICollectionViewDropProposal *)collectionView:(UICollectionView *)collectionView dropSessionDidUpdate:(id<UIDropSession>)session withDestinationIndexPath:(nullable NSIndexPath *)destinationIndexPath {
+    return [[UICollectionViewDropProposal alloc] initWithDropOperation:UIDropOperationMove intent:UICollectionViewDropIntentInsertAtDestinationIndexPath];
+}
+
 #pragma mark - Action Methods
 - (void)actionCreate:(id)sender {
-    
+
 }
 
 - (void)actionReorder:(UILongPressGestureRecognizer *)gestureRecognizer {
