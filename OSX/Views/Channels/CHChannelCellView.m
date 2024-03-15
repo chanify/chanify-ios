890a8290a67a1b6547135e93aed0c365fa9dc4f2
wizjin <wizjin@icloud.com>
2024-03-15 22:28:00 +0800

    fix: message title for osx.

--- OSX/Views/Channels/CHChannelCellView.m
+++ OSX/Views/Channels/CHChannelCellView.m
@@ -106,7 +106,7 @@
     
     NSString *mid = model.mid;
     CHMessageModel *m = [logic.userDataSource messageWithMID:mid];
-    self.detailLabel.text = m.summaryText;
+    self.detailLabel.text = m.title.length > 0 ? m.title : m.summaryText;
     self.dateLabel.text = [NSDate dateFromMID:m.mid].shortFormat;
     
     // TODO: Fix sync when receive push message.
